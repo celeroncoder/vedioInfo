ca2795147c41ccb531233dce4c4c1e0030a581bf
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-02-27 22:54:50 +0530

    took out IPC module of electoron

--- public/index.js
+++ public/index.js
@@ -1,6 +1,12 @@
 const form = document.querySelector("form");
 const fileSelector = document.querySelector("input");
 
+// cannot implement in simple browser but in a electron app it can compile common js module system.
+const electron = require("electron");
+
+// this is the module of IPC that enables us to communicate between the web window(renderer process) and the main electron app.
+const { ipcRenderer } = electron;
+
 form.addEventListener("submit", (event) => {
 	event.preventDefault();
 
